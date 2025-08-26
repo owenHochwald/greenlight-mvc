891a956b79f691d16093efd67a36bb832a301606
owenhochwald <owenhochwald@gmail.com>
2025-08-26 16:43:54 -0400

    add db connection and docker compose for postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -22,12 +30,27 @@ type application struct {
 const version = "1.0.0"
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	var cfg config
 
 	// flag setup and parsing
 	flag.IntVar(&cfg.port, "port", 8080, "port to run the server on")
-	flag.StringVar(&cfg.env, "env", "dev", "environment to run the server in")
+	flag.StringVar(&cfg.env, "env", os.Getenv("ENVIRONMENT"), "environment to run the server in")
+	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("DB_STRING"), "database connection string")
 	flag.Parse()
+
+	// connecting to database
+	db, err := ConnectDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to Postgres")
+
+	defer db.Close()
 
 	// setting up application and logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -37,10 +60,8 @@ func main() {
 		logger: logger,
 	}
 
-	// creating gin app
 	r := gin.Default()
 
-	// attaching custom error handler
 	r.Use(ErrorHandler())
 
 	r.NoRoute(func(c *gin.Context) {
@@ -52,4 +73,22 @@ func main() {
 	SetupRoutes(r, &app)
 
 	r.Run(fmt.Sprintf(":%d", cfg.port))
+}
+
+func ConnectDB(cfg config) (*sql.DB, error) {
+	var err error
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
